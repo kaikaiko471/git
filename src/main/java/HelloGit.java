ede8ca2505f5f012554cc041ae13301dc72f14ab
kaiko_glb <18202545015@163.com>
2021-06-05 21:30:23 +0800

    first commit

---
+++ src/main/java/HelloGit.java
@@ -0,0 +1,10 @@
+/**
+ * @author kaikaiko
+ * @create 2021-06-05 21:17
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("aaa");
+    }
+}
